bdb5c106982df0f7844b02ca7bcd4000147533f6
Chi <Chi@Lindos-MacBook-Air.local>
2020-01-09 15:23:01 +0200

    gallery outputs pics, need to sort for existing pics only and format output

--- controllers/Gallery.php
+++ controllers/Gallery.php
@@ -3,11 +3,19 @@ class Gallery extends Controller
 {
     public static function main_()
     {
-        $sth = static::query("SELECT post FROM camagru.posts WHERE user_id = 2");
-        //print_r($sth);
-        //print($sth[0][0]);
-        echo '<img src="./images/1576665043.png">';
-        echo '<img src="'.$sth[1][6].'">';
+        $sth = static::query("SELECT post FROM camagru.posts");
+        $i = 0;
+        $j = 0;
+        while ($i < 100)
+        {
+            $j = 0;
+            while ($j < 100)
+            {
+                echo '<img src="'.$sth[$i][$j].'">';
+                $j += 1;
+            }
+            $i += 1;
+        }
         //echo'<img src="data:image;base64,'.$sth['image'].'">';       
     }
 }
