1bb3b8ccf86f671037dc31c2e59f6952868d0537
lnkambul <lnkambul@student.wethinkcode.co.za>
2020-05-28 16:35:57 +0200

    global variable ftw

--- classes/ForgotPassword.php
+++ classes/ForgotPassword.php
@@ -4,15 +4,20 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+$change_id;
+
 class ForgotPassword extends Users
 {
     public static function main_()
     {
+        global $change_id;
         $redirect = FALSE;
         if (isset($_GET['poken']))
         {
             if (static::query('SELECT user_id FROM camagru.pokens WHERE poken=:poken', array(':poken'=>sha1($_GET['poken']))))
             {
+                $GLOBALS['change_id'] = static::query('SELECT user_id FROM camagru.pokens WHERE poken=:poken', array(':poken'=>sha1($_GET['poken'])))[0]['user_id'];
+                echo "changing password for user " . $change_id;
                 if (isset($_POST['changepassword']))
                 {
                     $newpword = $_POST['newpassword'];
@@ -22,8 +27,9 @@ class ForgotPassword extends Users
                         if(strlen($newpword) >= 8 && strlen($newpword) <= 30)
                         {
                             static::query('UPDATE camagru.users SET password=:newpassword WHERE id=:user_id',
-                                array(':newpassword'=>password_hash($newpword, PASSWORD_BCRYPT), ':user_id'=>static::isLoggedIn()));
+                                array(':newpassword'=>password_hash($newpword, PASSWORD_BCRYPT), ':user_id'=>$change_id));
                             echo "Password changed successfully";
+                            static::query('DELETE FROM camagru.pokens WHERE user_id=:user_id', array(':user_id'=>$change_id));
                             Home::main_();
                             $redirect = TRUE;
                         }
