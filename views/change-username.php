65e9338ba96e04d2304cc8011c6ff7cc17d78e94
lnkambul <lindonkambule@gmail.com>
2020-06-07 12:35:23 +0200

    Gallery[5/5] Editing[6/7] User[9/9] editing profile page, phantom span issue

--- views/change-username.php
+++ views/change-username.php
@@ -17,9 +17,9 @@
         <nav>
             <p> 
                 <ul>
+                    <li><a href="logout">logout</a></li>
                     <li><a href="gallery">gallery</a></li>
                     <li><a href="profile">profile</a></li>
-                    <li><a href="logout">logout</a></li>
                     <li><a href="settings">settings</a></li>
                     </ul>
             </p>
